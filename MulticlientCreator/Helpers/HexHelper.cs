e58a0e7ea05caeed42726aea0a98bdc4de98f5e6
Fizo55 <hugomacees56000@gmail.com>
2021-04-18 15:18:21 +0200

    Add basic Deserialization

---
+++ MulticlientCreator/Helpers/HexHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace MulticlientCreator.Helpers
+{
+    public static class HexHelper
+    {
+        public static byte[] ToByteArray(string hex)
+        {
+            var charNumber = hex.Length;
+            var bytes = new byte[charNumber / 2];
+            for (var i = 0; i < charNumber; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        public static string ToHexString(byte[] barray)
+        {
+            var c = new char[barray.Length * 2];
+            for (var i = 0; i < barray.Length; ++i)
+            {
+                var b = ((byte)(barray[i] >> 4));
+                c[i * 2] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+                b = ((byte)(barray[i] & 0xF));
+                c[i * 2 + 1] = (char)(b > 9 ? b + 0x37 : b + 0x30);
+            }
+            return new string(c);
+        }
+    }
+}
---
+++ MulticlientCreator/Program.cs
@@ -0,0 +1,28 @@
+﻿using MulticlientCreator.Helpers;
+using System;
+using System.IO;
+using System.Text;
+
+namespace MulticlientCreator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            boucle:
+            // C:\Users\HP Gaming Pavilion\Desktop\NosAdventure
+            Console.WriteLine("Enter your nostale path :");
+            var nostalePath = Console.ReadLine();
+
+            if (nostalePath == null)
+                goto boucle;
+
+            string finalPath = @$"{nostalePath}\NostaleClientX.exe";
+            Stream fs = new FileStream(finalPath, FileMode.Open, FileAccess.Read);
+            byte[] getFileBytes = DeserializationHelper.ReadFully(fs);
+            string getFileString = HexHelper.ToHexString(getFileBytes);
+            Console.WriteLine(getFileString);
+            Console.ReadLine();
+        }
+    }
+}
